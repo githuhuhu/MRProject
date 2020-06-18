a15c0a274b330d346bcf025646a3979eb286a271
githuhuhu <sjswld20@gmail.com>
2020-06-18 15:00:39 +0900

    Project Upload 커밋주석입니다

---
+++ SmartGlass/Assets/Scripts/Controller.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    private Camera arCamera;
+    private RaycastHit hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //터치(클릭)
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Touched");
+        }
+
+        //터치
+        if(Input.GetMouseButton(0))
+        {
+            Debug.Log($"Touching {Time.time}");  // 아래거랑 같은거~
+            // Debug.Log("Touching" + Time.time);
+        }
+
+
+       
+
+        // 터치 좌표값
+        Vector2 pos = Input.mousePosition;
+
+        // 스크린 터치좌표값을 3차원 Ray 생성
+        Ray ray = arCamera.ScreenPointToRay(pos);
+
+        if(Input.GetMouseButtonDown(0) && Physics.Raycast(ray, out hit, 10.0f, 1<<8))
+        {
+            // Debug.Log($"Position x={pos.x}, y={pos.y}");
+            hit.collider.GetComponent<Renderer>().material.color = Color.red;
+        }
+
+        // 화살표 입력값
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Debug.Log("Click Left Arrow");
+        }
+        
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Debug.Log("Click Right Arrow");
+        }
+
+
+
+
+    }
+}
---
+++ test.md
@@ -0,0 +1,46 @@
+
+*Markdown 이라는 사용법을 반드시 배워야 한다. 
+개발자들 메뉴얼이라든가 다 이걸 쓰고, 
+유니티 메뉴얼도 마크다운을 쓰고 있다. 
+https://unity3dstudy.com/ 참조사이트
+
+검색 마크다운 문법
+https://www.google.com/search?q=%EB%A7%88%ED%81%AC%EB%8B%A4%EC%9A%B4%EB%AC%B8%EB%B2%95&oq=%EB%A7%88%ED%81%AC%EB%8B%A4%EC%9A%B4%EB%AC%B8%EB%B2%95&aqs=chrome..69i57.4826j1j7&sourceid=chrome&ie=UTF-8
+
+자주쓰는 마크다운 문법
+
+# HEAD1
+
+#유니티 엔진 소게
+
+## 게임엔진의 특징
+-멀티플랫폼 빌드
+	- ㅁㅁㅁ
+	- 아아앙
+	- 라아앙
+-모바일빌드
+
+
+---
+private Transform tr;
+
+```
+cs
+private Start()
+{
+	tr = this.gameObject.GetComponent<Transform>90;
+}
+```
+
+
+| 제목     | 주제         |
+| -------- | ------------ |
+| 컴포넌트 | 컴포넌트란 ? |
+| 컴포넌트 | 컴포넌트란 ? |
+| 컴포넌트 | 컴포넌트란 ? |
+
+
+
+## HEAD2
+
+### HEAD3
